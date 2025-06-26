c3bdadc40b65bf6bbc4c64dcfd663c29cec53a28
Osama Alali <osama@example.com>
2025-06-26 05:18:51 +0300

    add root Project

---
+++ SUQEAA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SUQEAA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
