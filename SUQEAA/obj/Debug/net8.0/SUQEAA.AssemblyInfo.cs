c45a4e48e5f669da22ec827b818d454002f6011a
Karem <abdalkremhsn2002@gmail.com>
2025-08-01 02:37:04 +0300

    Add code the Distributors Dashboard

--- SUQEAA/obj/Debug/net8.0/SUQEAA.AssemblyInfo.cs
+++ SUQEAA/obj/Debug/net8.0/SUQEAA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SUQEAA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e48e440eca89cfdeae98f312ec534e32b53b1b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043e48f37ee0f3f642199d7db8257295564a78d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SUQEAA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SUQEAA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
